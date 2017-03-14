9a55563dcb2414ea9e25702fa33a2a19c06444f8
thegrayninja <grayninja@protonmail.com>
2017-03-14 15:44:54 -0700

    Update delete_rows_v2.py

--- delete_rows_v2.py
+++ delete_rows_v2.py
@@ -15,18 +15,21 @@ from sys import argv
 #opens your csv file and saves the good stuff to x
 x = ""
 with open(argv[1], 'rb') as csvfile:
+		
 	delimfile = csv.DictReader(csvfile)
 	for row in delimfile:
+
 
 		if 'None' in row['Risk']:
 			print ('None')
 		else:
-			x = x + '%s,%s\n' %(row['Host'], row['Risk'])
+			x = x + '%s,%s,%s,%s,%s,%s,%s\n' %(row['Host'], row['Risk'], row['CVSS'], row['CVE'], row['Port'], row['Protocol'], row['Name'])
 csvfile.close()
 
 
 #to save the good stuff to a new file
 with open(argv[2], 'a') as csvnew:
+	csvnew.write('Host, Risk, CVSS, CVE, Port, Protocol, Name\n')
 	csvnew.write(x)
 
 csvnew.close()
