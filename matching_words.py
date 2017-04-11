8d4204b24ee0a8713fb140c41052c5abf54df434
thegrayninja <grayninja@protonmail.com>
2017-04-10 22:14:23 -0700

    Update matching_words.py

--- matching_words.py
+++ matching_words.py
@@ -1,33 +1,29 @@
+#github.com/thegreyinja
 first_word = raw_input("Please enter word #1: ")
 second_word = raw_input("Please enter word #2: ")
 
 def compare_words(first_string, second_string):
 	dict1 = {}
 	dict2 = {}
-  
-	count = 0
-	for i in first_string:
-		if i in dict1:
-			count +=1	
-			dict1[i] = count
-		else:
-			count = 1
-			dict1[i] = count
 
-	re_count = 0
-	for i in second_string:
-		if i in dict2:
-			re_count +=1	
-			dict2[i] = re_count
+	for i in first_string:
+		if i in dict1:	
+			dict1[i] += 1
 		else:
-			re_count = 1
-			dict2[i] = re_count
+			dict1[i] = 1
+
+	for i in second_string:
+		if i in dict2:	
+			dict2[i] += 1
+		else:
+			dict2[i] = 1
 
 
 	if dict1 == dict2:
 		print ("%s and %s contain the same letters!" % (first_word,second_word))
 	else:
 		print ("%s and %s DO NOT contain the same letters :(" % (first_word,second_word))
-
+	#remove next comment for debugging! :)	
+	#print ("%s and %s" % (dict1, dict2))
 
 compare_words(first_word.lower(),second_word.lower())
