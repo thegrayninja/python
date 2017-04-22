db0a042e70a8af296c85c6a509f1d00bbcb437bb
thegrayninja <grayninja@protonmail.com>
2017-04-22 04:32:13 +0000

    Update prime_numbers.py

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,6 +1,5 @@
 #github.com/thegrayninja
 ##prime number
-##warning - 7+ digits puts a stress on your memory
 
 x_input = raw_input("Please enter a number: ")
 x = int(x_input)
@@ -8,13 +7,14 @@ primeno = 0
 divide_by = x
 divisible_numbers = ""
 
-for i in range(x):
-	i2 = i+1
-	if i2 != 1:
-		if i2 != x:
-			if x % i2 == 0:
-				divisible_numbers += "%d is divisible by %d.\n" % (x, i2)
-				primeno = 1
+i = 1
+while i < ((x/2)+1):
+	if i != 1:
+		print(i)
+		if x % i == 0:
+			divisible_numbers += "%d is divisible by %d.\n" % (x, i)
+			primeno = 1
+	i +=1
 			
 
 if primeno == 1:
