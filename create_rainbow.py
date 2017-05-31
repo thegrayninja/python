1d09eaf9a98568c18879ae7de5df9309834d3d54
thegrayninja <grayninja@protonmail.com>
2017-05-31 09:47:19 -0700

    Update create_rainbow.py
    'hopefully' fixed memory error. Removed the need to save to a variable first.

--- create_rainbow.py
+++ create_rainbow.py
@@ -4,14 +4,12 @@
 import itertools
 
 alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*(),./<>?;':[]{}-=_+` \|\""
+#alphabet = "test"
 pwlength = int(raw_input("Please enter the password length: "))
-count = 0
-x=""
-total = 0
+
+
+updatedFile = open("all_combinations_pw_%s.txt" % (str(pwlength)), "a") #creates new file
 for combination in itertools.product(alphabet, repeat=pwlength):
-    x += ''.join(map(str, combination))+'\n'
-
-
-updatedFile = open("all_combinations_pw_%s.txt" % (str(pwlength)), "w") #creates new file
-updatedFile.write(x)		#then writes to file
+    updatedFile.write(''.join(map(str, combination))+'\n')		#then writes to file
+	
 updatedFile.close()
