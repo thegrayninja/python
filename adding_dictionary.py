fa14aa7948e87c4dcc73dee787b725d50158c8c4
thegrayninja <grayninja@protonmail.com>
2018-01-17 11:20:09 -0800

    Update adding_dictionary.py

--- adding_dictionary.py
+++ adding_dictionary.py
@@ -8,3 +8,21 @@ def addition(nums, target):
 			print (result_dict[nums[i]], i)
 
 addition([7,10,13,55,68,8,3,2,4],17)
+
+
+
+##wrote this up for a future project
+Platforms = {}
+
+SampleData = ["People","Dogs","People","Cats","Car","Cats",""]
+SampleData2 = ["Psycho","Addict","Insane","Cats","Car","Cats",""]
+
+
+for i in SampleData:
+    i = i.strip()
+    if i not in Platforms:
+        Platforms[i] = {"assets":[i]}
+    else:
+        Platforms[i]["assets"].append(i)
+
+print(Platforms)
