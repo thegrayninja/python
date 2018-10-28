89e5bcb571b4a2beaa50d82ea9592c9c13136a16
thegrayninja <grayninja@protonmail.com>
2018-10-27 22:26:25 -0700

    Update random_password.py

--- random_password.py
+++ random_password.py
@@ -1,34 +1,33 @@
-#github.com/thegreyninja
-#not quite random as you can see..but it's a start
-
 import random
-from array import *
 
 
-def random_password():
-	alphabet_lower = "abcdefghijklmnopqrstuvwxyz!"
-	alphabet_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	alphabet_special = "@#$%^&*(),./<>?;':[]{}-=_"
-	alphabet_number = "1234567890"
+def PasswordGeneratorMain(PWLength):
+    alphabet_lower = "abcdefghijklmnopqrstuvwxyz!"
+    alphabet_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    alphabet_special = "@#$%^&*(),./<>?;':[]{}-=_"
+    alphabet_number = "1234567890"
 
-	random_pw = []
-	count = 0
-	while count < 16:
-		value = random.randrange(len(alphabet_lower))
-		random_pw.insert(count,alphabet_lower[value])
-		count += 1
-		value = random.randrange(len(alphabet_upper))
-		random_pw.insert(count,alphabet_upper[value])
-		count += 1
-		value = random.randrange(len(alphabet_special))
-		random_pw.insert(count,alphabet_special[value])
-		count += 1
-		value = random.randrange(len(alphabet_number))
-		random_pw.insert(count,alphabet_number[value])
-		count += 1
-	
-	
-	random.shuffle(random_pw)
-	print (''.join(random_pw))
+    total_search = alphabet_lower + alphabet_upper + alphabet_special + alphabet_number
+    random_pw = []
 
-random_password()
+    PWLength = int(PWLength)
+
+    value = random.randrange(len(alphabet_lower))
+    random_pw.insert(0, alphabet_lower[value])
+    value = random.randrange(len(alphabet_upper))
+    random_pw.insert(1, alphabet_upper[value])
+    value = random.randrange(len(alphabet_special))
+    random_pw.insert(2, alphabet_special[value])
+    value = random.randrange(len(alphabet_number))
+    random_pw.insert(3, alphabet_number[value])
+
+    count = 4
+    while count < PWLength:
+        value = random.randrange(len(total_search))
+        random_pw.insert(count, total_search[value])
+        count += 1
+
+
+    random.shuffle(random_pw)
+    FinalPW = ''.join(random_pw)
+    return FinalPW
