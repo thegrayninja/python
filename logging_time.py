42e0b5d2f0f1715b0fb3888bfda6a40b07aa61f9
grayninja <grayninja@protonmail.com>
2018-11-08 12:49:00 -0800

    updated formating styles to logging_time.py

--- logging_time.py
+++ logging_time.py
@@ -8,3 +8,25 @@ def GetCurrentTime():
     
 #returns HH:MM:SS MM:DD:YY TimeZone
 
+#print(GetCurrentTime())
+
+
+
+#import datetime
+
+#
+# newtime = "2018-09-25T16:49:38.000+0000"
+#
+# LastScannedTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(newtime))
+
+# print(newtime)
+#
+# #print(time.strftime('%X %x %Z', newtime))
+#
+# datetime_object = datetime.strptime(newtime, '%b %d %Y %I:%M%p')
+
+
+import datetime
+lastScannedTime = "2018-09-25T16:49:38.000+0000"
+finalTime = datetime.datetime.strptime(lastScannedTime, "%Y-%m-%dT%H:%M:%S.000+0000").strftime('%B %d, %Y %I:%M %p')
+print(finalTime)
